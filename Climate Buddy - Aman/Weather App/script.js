5594ea28e68cd64f0913ebb5732260529cf40b42
Aman Singh <133858133+iamanhere@users.noreply.github.com>
2023-09-24 16:04:19 +0530

    script.js

--- Climate Buddy - Aman/Weather App/script.js
+++ Climate Buddy - Aman/Weather App/script.js
@@ -38,19 +38,19 @@ async function checkWeather(city){
 
     switch(weather_data.weather[0].main){
         case 'Clouds':
-            weather_img.src = "/Weather App/Weather app img/cloud.png";
+            weather_img.src = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhg274WYHgnSu3PsYLzdhVMwjWRgKSDt2io9g6IrEFJZ45w1l9jv5Ii_ogoS3BS5P-nEmwzP9j2BTd4rIaA41QgPPiQdJBtHU8wuGvolFrcYLWtYKcc6AOAUkM8gitiHnos_939VfaPSe9RJMecm-jt9RjZk53rzXRlslTRTtMOfOrl3a1KvyghZsR0ujk/s16000/cloud.png";
             break;
         case 'Clear':
-            weather_img.src = "/Weather App/Weather app img/clear.png";
+            weather_img.src = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhYeQzOheP3IaNNNJgROAn5-vb687ze-KCrrpcBPPqh1c_uAdQAAPugtfOy9bs5YraSBv66mzvV8RrWBqA65dwu0w4waIXXRdJoht6LBkiq04ISLGnMXuic0AT4OYoM-B6A0iRtwy-bqci3womVxIvb_bnvvTrSHZgGhCedbMQ5OGWDFGmIGIL0yuuazXs/s16000/clear.png";
             break;
         case 'Rain':
-            weather_img.src = "/Weather App/Weather app img/rain.png";
+            weather_img.src = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj7N5xr5syZT6sk5ryKtTsEech53wsin_j6K1PZnEPTh_K20lHnvfjVr4q4SViQbE9qzZmHm60oDJMAooQXUqKaULGp17H9b7B2D8l52D0qOXxk1tjtQadQV6mHRROd-4_9y2oEGE7x8BWhLjfTeSSKUbq_-Pnz9S7G7JnYOUEeBe-UmPKQhsDXWTBZ2RI/s16000/rain.png";
             break;
         case 'Mist':
-            weather_img.src = "/Weather App/Weather app img/mist.png";
+            weather_img.src = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgkVpOHdQYsdZfbbVagEUMxvu8KwEAkeW3HZs87YM-qweOcKzO3tZu_HtMwjavJbONrdvqtaW7WKtH3lucgOC3gIGZpabhWXp7dWW2Xhczsx6bZilnYI6fwJSuqCzE_jxBMHxbGwaZ8kpZkNKXbGjpwKtt0tQUylZDEoeYSL5my-oUpT7wRhD5u3lyFiVk/s16000/mist.png";
             break;
         case 'Snow':
-            weather_img.src = "/Weather App/Weather app img/snow.png";
+            weather_img.src = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhCiR4u4iBO5oTHPSKeU-0m-BtP-WoEe_Drmr72FvVwSs9pZKD3fHhEtidEE6KgYDoyzOBjv5U_iL0iW522Zd4hNEqFDryE6j7zIxRvULXm6Z2DWBCrY0nN7q0ktF5kI8-u91bcNks8COgswhuuBKRFmnvMObYKZ6wIOEI1R-GR4Vre8uRrkr1xYtkpdOk/s16000/snow.png";
             break;
 
     }
